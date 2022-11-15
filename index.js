aad49e94dc5f2c73ec7a5ba67b76fbe8ee8de412
Nicole Robbins <nicole.robbins@att.net>
2022-11-14 18:46:37 -0600

    commit tree 9e68bbb8865437f268b23aaa3451406dbe3f40dc

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+ const reversedWord = reverseString(word);
+ return word === reversedWord;
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
-  Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
-  Add written explanation of your solution here
+  // create an array from the input string
+  // reverse the array
+  // create a string from the reversed array
+  // return the reversed string
+
+  // reverse the input string
+  // if the reversed string is the same as the input
 */
 
 // You can run `node index.js` to view these console logs
